9a019d57b1b1ce22065e7a8a69e691737cef8177
ThememoriesIII <ThememoriesIII@users.noreply.github.com>
2019-09-17 21:00:24 +0700

    seccon release

---
+++ door_Auto_with_class.ino
@@ -0,0 +1,102 @@
+#include <HCSR04.h>
+
+#define tpin 9 //ขาทริก
+#define epin 10 //ขาแอคโค้
+#define Sdoor_close 5 //door pin ขาสวืทฝั่งปิด
+#define Sdoor_open 6 //door pin ขาสวิทฝั่งเปิด
+#define motorA 4 //ขามอเตอร์ฝั่งบวก
+#define motorB 3 //ขามอเตอร์ฝั่งลบ
+
+#define Max_dis 100 //ค่าระยะสูงสุดที่คนเดินเข้ามา
+#define Low_dis 10 //ค่าระยะต่ำสุดที่คนเดืนเข้า
+#define time_check_door 200 //เวลาเช็คสถานะประตู เช็คทุก 200 มิลลิวิ
+#define time_check_SW 50 //เวลาเช็คสถานะประตู เช็คทุก 50 มิลลิวิ
+#define time_delay_door 3000 //เวลาเช็คสถานะประตู เช็คทุก 3 วิ
+
+class Doorcontrol{
+  private:
+    byte pN;
+    byte pP;
+  public:
+  enum Door{dooropen,doorclose,doorstop};
+    Doorcontrol(byte a,byte b){
+      pN=a;
+      pP=b;
+    }
+    ~Doorcontrol();
+    void begin(){
+      pinMode(pinB);
+    }
+    void drive(Door control){
+      switch(){
+        case dooropen:
+        digitalWrite()
+      }
+    }
+};
+
+HCSR04 Sensor_door(tpin,epin);
+
+float messur_dist = 0; //เก็บระยะทาง
+unsigned long time1 = 0; //เก็บระยะเวลาที่ฟังชั่นทำงาน
+unsigned long time2 = 0; //เก็บระยะเวลาที่ฟังชั่นทำงาน
+unsigned long doorTime = 0; //เก็บระยะเวลาประตูเปิด
+void setup() {
+  pinMode(Sdoor_close,INPUT);
+  pinMode(Sdoor_open,INPUT);
+}
+
+void loop() {
+  if(millis()-time2 >= time_check_SW)
+  {
+     sw_door();
+     time2 = millis();
+  }
+  if(millis()-time1 >= time_check_door)
+  {
+     door_control();
+     time1 = millis();
+  }
+}
+
+void door_control()
+{
+     
+    messur_dist = Sensor_door.dist();
+    //ถ้าคนอยู่ในระยะ 10 ถัง 100เมตร กับประตูปิด ให้เปิดประตู
+    if((messur_dist>Low_dis&&messur_dist<Max_dis)&&door==doorclose)
+    {
+      openAdoor();
+      doorTime = millis();
+    }
+    else
+    {
+      //ถ้าประตูเปิดอยู่แล้วเปิดเกิน 3 วินาทีสั่งปิด
+      if(door==dooropen&&millis()-doorTime >= time_delay_door)
+        closeAdoor();
+      else
+        //ถ้าประตูปิดแล้ว และ คนไม่อยู่ในระยะให้หยุดมอเตอร์
+        stopAdoor();
+    }
+}
+
+void sw_door()
+{
+  door = (digitalRead(Sdoor_close)==HIGH&&digitalRead(Sdoor_open)==LOW)?doorclose:dooropen;
+}
+
+void openAdoor()
+{
+  digitalWrite(motorA,HIGH);
+  digitalWrite(motorB,LOW);
+}
+void closeAdoor()
+{
+  digitalWrite(motorA,LOW);
+  digitalWrite(motorB,HIGH);
+}
+void stopAdoor()
+{
+  digitalWrite(motorA,LOW);
+  digitalWrite(motorB,LOW);
+}
